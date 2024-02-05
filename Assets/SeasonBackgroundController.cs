ac336b69216253dae6799c8d9101b21898fedbbc
Mario Gunadi <147931463+veempeelge@users.noreply.github.com>
2024-02-05 11:30:03 +0700

    smth

--- Assets/SeasonBackgroundController.cs
+++ Assets/SeasonBackgroundController.cs
@@ -8,18 +8,29 @@ public class SeasonBackgroundController : MonoBehaviour
 
     [SerializeField] GameObject backgroundColor;
     [SerializeField] Material[] bgColor;
+    [SerializeField] Material[] groundMaterial;
     [SerializeField] string[] seasonText;
     [SerializeField] TMP_Text season;
-     Material whatMaterial;
-     Material currentMaterial;
-   
+
+    Material whatMaterial;
+    Material whatGroundMaterial;
+    [SerializeField] GameObject ground;
+    Material currentGroundMaterial;
+    Material currentMaterial;
+
     // Start is called before the first frame update
     void Start()
     {
-        int index = Random.Range(0,bgColor.Length);
+        int index = Random.Range(0, bgColor.Length);
+
         backgroundColor = this.gameObject;
+
         currentMaterial = backgroundColor.GetComponent<Material>();
         whatMaterial = bgColor[index];
+
+        currentGroundMaterial = ground.GetComponent<Material>();
+        whatGroundMaterial = groundMaterial[index];
+
         season.SetText(seasonText[index]);
     }
 
@@ -27,15 +38,29 @@ public class SeasonBackgroundController : MonoBehaviour
     void Update()
     {
         ChangeObjectMaterial(whatMaterial);
-        
+        ChangeGroundMaterial(whatGroundMaterial);
     }
 
-    
+
 
     // Function to change the material
     public void ChangeObjectMaterial(Material material)
     {
         Renderer renderer = GetComponent<Renderer>();
+
+        if (renderer != null && material != null)
+        {
+            renderer.material = material;
+        }
+        else
+        {
+            Debug.LogError("Renderer or newMaterial is null. Make sure to assign a material and attach this script to a GameObject with a Renderer component.");
+        }
+    }
+
+    public void ChangeGroundMaterial(Material material)
+    {
+        Renderer renderer = ground.GetComponent<Renderer>();
 
         if (renderer != null && material != null)
         {
